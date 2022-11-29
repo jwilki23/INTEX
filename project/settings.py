e44a364bc21e07ebc4eff4098e4a788ae7209cfe
ajp86 <paulsenjohn00@gmail.com>
2022-11-29 11:17:27 -0700

    settings migration

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nkf',
         'USER' : 'postgres',
-        'PASSWORD' : '',
+        'PASSWORD' : 'Apau7101!',
         'HOST' : 'localhost'
     }
 }
