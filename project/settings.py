aea6d11de2c36941d395866b0c9a1a5f8f52dbc4
Bennett McCullough <mccullough.bennett@gmail.com>
2022-11-29 12:00:43 -0700

    adminpassword

--- project/settings.py
+++ project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nkf',
         'USER' : 'postgres',
-        'PASSWORD' : 'kindlefireadmin',
+        'PASSWORD' : 'admin',
         'HOST' : 'localhost'
     }
 }
