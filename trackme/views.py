edf3744437ca0d62047e4ce9c36106f2c2bd34de
ajp86 <paulsenjohn00@gmail.com>
2022-11-29 17:02:54 -0700

    Crudstart

--- trackme/views.py
+++ trackme/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from .models import Person
+
 
 # Create your views here.
 
@@ -16,4 +21,27 @@ def loginPageView(request) :
     return render(request, 'trackme/login.html')
 
 def signupPageView(request) : 
-    return render(request, 'trackme/signup.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            user = Person()
+
+            user.first_name = request.POST['first_name']
+            user.last_name = request.POST['last_name']
+            user.age = request.POST['age']
+            user.gender = request.POST['gender']
+            user.weight = request.POST['weight']
+            user.height_feet = request.POST['height_feet']
+            user.height_inches = request.POST['height_inches']
+
+            user.save()
+    
+    context = {
+        'form': form,
+    }
+    return render(request, 'trackme/signup.html', context)
+
