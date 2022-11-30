53ba52ae1bdfe41c75d5f272e4aa168ade770e11
ajp86 <paulsenjohn00@gmail.com>
2022-11-30 10:27:55 -0700

    runningshit

--- trackme/views.py
+++ trackme/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
-from .models import Person
-
+from .models import Person, Stage, Morbidity
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -18,30 +19,56 @@ def myDataPageView(request) :
     return render(request, 'trackme/mydata.html')
 
 def loginPageView(request) : 
-    return render(request, 'trackme/login.html')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('mydata')
+        else:
+            messages.info(request, "Username or Password is incorrect")
+
+    context = {}
+    return render(request, 'trackme/login.html', context)
 
 def signupPageView(request) : 
-    form = UserCreationForm()
-
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
+            person = Person()
+            form_user = form.cleaned_data.get('username')
 
-            user = Person()
+            person.first_name = request.POST['first_name']
+            person.last_name = request.POST['last_name']
+            person.age = request.POST['age']
+            person.gender = request.POST['gender']
+            person.weight = request.POST['weight']
+            person.height_feet = request.POST['height_feet']
+            person.height_inches = request.POST['height_inches']
+            # user.stage = request.POST['stage']
 
-            user.first_name = request.POST['first_name']
-            user.last_name = request.POST['last_name']
-            user.age = request.POST['age']
-            user.gender = request.POST['gender']
-            user.weight = request.POST['weight']
-            user.height_feet = request.POST['height_feet']
-            user.height_inches = request.POST['height_inches']
+            stage_id = request.POST['stage']
+            assign_stage = Stage.objects.get(id=stage_id)
+            person.stage = assign_stage
+            # person.morbidity_type = request.POST['morbidity_type', False]
 
-            user.save()
-    
-    context = {
-        'form': form,
-    }
-    return render(request, 'trackme/signup.html', context)
+            person.save()
+            messages.success(request, 'Account was created for ' + form_user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        stage = Stage.objects.all()
+        morbidity = Morbidity.objects.all()
+
+        context = {
+            'form': form,
+            'stage' : stage,
+            'morbidity': morbidity
+        }
+        return render(request, 'trackme/signup.html', context)
 
