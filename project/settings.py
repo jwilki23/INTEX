1d8b2d5540dd56aa6808b3d17693a908462e27ca
ajp86 <paulsenjohn00@gmail.com>
2022-12-01 20:33:14 -0700

    colorchanges

--- project/settings.py
+++ project/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'nkf',
         'USER' : 'postgres',
-        'PASSWORD' : 'admin',
+        'PASSWORD' : 'Apau7101!',
         'HOST' : 'localhost'
     }
 }
